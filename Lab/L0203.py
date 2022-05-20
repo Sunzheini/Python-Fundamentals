73ea050d64c027fe54fd38c1ec9af498a5f7f012
Sunzheini <daniel_zorov@abv.bg>
2022-05-20 17:13:51 +0300

    second commit

--- Lab/L0203.py
+++ Lab/L0203.py
@@ -16,16 +16,16 @@
 
 #-------------------------------------------------------------------------------------
 # 1
-# data = input().split(' ')
-# bakery = {}
-#
-# for i in range(0, len(data), 2):
-#     food = data[i]
-#     quantity = int(data[i+1])
+data = input().split(' ')
+bakery = {}
 
-#     bakery[food] = quantity
-#
-# print(bakery)
+for i in range(0, len(data), 2):
+    food = data[i]
+    quantity = int(data[i+1])
+
+    bakery[food] = quantity
+
+print(bakery)
 
 #-------------------------------------------------------------------------------------
 #iteration with keys()
